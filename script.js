14d8afbd465703d91c60d73a1819eb21cfbb645b
matheus <mateakys@gmail.com>
2025-11-07 15:40:10 -0300

    feat: mousedown && mouseenter

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const btn = document.querySelector('#btn');
 const rgbBtn = document.querySelector('#rgb');
 const opacityBtn = document.querySelector('#opacity');
 const deleteBtn = document.querySelector('#delete');
+const eraseBtn = document.querySelector('#erase');
 const size = document.querySelector('#size');
+const color = document.querySelector('#color');
 const shadowAll = document.querySelector('.grid div');
 
+size.addEventListener('change', () => {
+  const dimension = document.querySelector('#dimension');
+  const text = size.value;
+  dimension.textContent = `${text}x${text}`;
+});
+
 let deleteToggled = false;
+let eraseToggled = false;
 let opacityToggled = false;
 let rgbToggled = false;
+let mouseDown = false;
+
+document.body.addEventListener("mousedown", e => mouseDown = true);
+document.body.addEventListener("mouseup", e => mouseDown = false);
+
+
 let gridSize = 16;
 btn?.addEventListener('click', () => {
   const grid = document.querySelector('.grid');
@@ -25,32 +40,33 @@ rgbBtn?.addEventListener('click', () => {
   } else {
     rgbBtn.classList.remove('toggled'); 
   }
-  containerGrid?.removeChild(grid);
-  newGrid();
+});
+
+eraseBtn?.addEventListener('click', () => {
+  eraseToggled = !eraseToggled;
+  if (eraseToggled) {
+    eraseBtn.classList.add('toggled'); 
+  } else {
+    eraseBtn.classList.remove('toggled'); 
+  }
 });
 
 opacityBtn?.addEventListener('click', () => {
-  const grid= document.querySelector('.grid');
   opacityToggled = !opacityToggled;
   if (opacityToggled) {
     opacityBtn.classList.add('toggled'); 
   } else {
     opacityBtn.classList.remove('toggled'); 
   }
-  containerGrid?.removeChild(grid);
-  newGrid();
 });
 
 deleteBtn?.addEventListener('click', () => {
-  const grid= document.querySelector('.grid');
   deleteToggled = !deleteToggled;
   if (deleteToggled) {
     deleteBtn.classList.add('toggled'); 
   } else {
     deleteBtn.classList.remove('toggled'); 
   }
-  containerGrid?.removeChild(grid);
-  newGrid();
 });
 
 function newGrid() {
@@ -69,28 +85,40 @@ function newGrid() {
       newDiv.classList.add('grid-element');
       newDiv.style.width = cellSize + 'px';
       newDiv.style.height = cellSize + 'px';
-      const red = Math.floor(Math.random() * 256);
-      const green = Math.floor(Math.random() * 256);
-      const blue = Math.floor(Math.random() * 256);
-      newDiv.addEventListener('mouseover', () => {
-        let currentOpacity = parseFloat(newDiv.style.opacity) || 0;
-        currentOpacity = Math.min(1, currentOpacity + 0.1);
-        if (rgbToggled) {
-          newDiv.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-        } else {
-          newDiv.style.backgroundColor = 'black';
-        }
-        if (opacityToggled) {
-          newDiv.style.opacity = String(currentOpacity);
-        } else {
-          newDiv.style.opacity = '1';
-        }
-        if (deleteToggled) {
-          const everything = document.querySelectorAll('.grid div');
-          everything.forEach((element) => {
-            element.classList.add('shadowed');
-          });
-          newDiv.style.opacity = '0';
+      newDiv.addEventListener('mouseover', (e) => {
+        if(e.type == "mousedown" || (e.type == "mouseover" && mouseDown)){
+          const red = Math.floor(Math.random() * 256);
+          const green = Math.floor(Math.random() * 256);
+          const blue = Math.floor(Math.random() * 256);
+          let currentOpacity = parseFloat(newDiv.style.opacity) || 0;
+          currentOpacity = Math.min(1, currentOpacity + 0.1);
+          if (eraseToggled) {
+            newDiv.style.backgroundColor = 'white';
+            newDiv.style.opacity = '0';
+          } else {
+            if (rgbToggled) {
+              newDiv.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            } else {
+              newDiv.style.backgroundColor = color.value;
+            }
+          }
+          if (opacityToggled) {
+            newDiv.style.opacity = String(currentOpacity);
+          } else {
+            newDiv.style.opacity = '1';
+          }
+          if (deleteToggled) {
+            const everything = document.querySelectorAll('.grid div');
+            everything.forEach((element) => {
+              element.classList.add('shadowed');
+            });
+            newDiv.style.opacity = '0';
+          } else {
+            const everything = document.querySelectorAll('.grid div');
+            everything.forEach((element) => {
+              element.classList.remove('shadowed');
+            });
+          }
         }
       });
       row.appendChild(newDiv);
